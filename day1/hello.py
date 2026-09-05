1f6667456d22e60e4ecb1839c5ddbd45879d60c0
P Phiraphat <pphiraphat.b@gmail.com>
2026-09-05 23:43:18 +0700

    day 1: python basic

---
+++ day1/hello.py
@@ -0,0 +1,14 @@
+print("Hey everyone, My name is P Phiraphat.")
+print("I was replaced by ChatGPT before I even got hired and in six months, I won't even have an internship place to go,")
+print("I've decided to master the basics — so at least I can be the guy who prompts the AI to fix its own bugs.")
+
+print("1") #text string
+print(1) #number same the result but different type
+
+# print("unemployed") #this is a comment, it will not be executed by the interpreter
+
+#excercise 1
+print(10+5)
+print(10-5)
+print(10*5)
+print(10/5)
