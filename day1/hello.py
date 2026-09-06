f60b74e8e4f0eac796d425a39b74e30941573a4b
P Phiraphat <pphiraphat.b@gmail.com>
2026-09-06 14:05:56 +0700

    update day 1: output & comment

--- day1/hello.py
+++ day1/hello.py
@@ -2,12 +2,13 @@ print("Hey everyone, My name is P Phiraphat.")
 print("I was replaced by ChatGPT before I even got hired and in six months, I won't even have an internship place to go,")
 print("I've decided to master the basics — so at least I can be the guy who prompts the AI to fix its own bugs.")
 
-print("1") #text string
-print(1) #number same the result but different type
+print("1") #string
+print(1) #number
+#in line 5 and 6 is the sane result, but the data type is different. The first one is a string and the second one is a number.
 
 # print("unemployed") #this is a comment, it will not be executed by the interpreter
 
-#excercise 1
+#excercise
 print(10+5)
 print(10-5)
 print(10*5)
