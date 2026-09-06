b02140cfb2a83fd480f7ea55e6c8ad7f8b42b485
P Phiraphat <pphiraphat.b@gmail.com>
2026-09-06 20:54:51 +0700

    update day 2: if-else & ternary operator

---
+++ day2/ternary-operator.py
@@ -0,0 +1,12 @@
+# ternary operator = if-else แบบลดรูป
+
+# การเขียนโปรแกรมคำนวรณเลขคุ่หรือเลขคี่
+number = int(input("Enter a number: "))
+print("your number is:", number)
+
+# if number % 2 == 0:
+#     print("your number is even") # เลขคู่
+# else:
+#     print("your number is odd") # เลขคี่
+
+print("number is even") if number % 2 == 0 else print("number is odd") # การเขียนแบบ ternary operator
