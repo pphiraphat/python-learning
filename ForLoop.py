1762f2742172911bd746ac2e3e5a73a815ab48e0
P Phiraphat <pphiraphat.b@gmail.com>
2026-09-10 15:08:22 +0700

    Learning For Loop

---
+++ ForLoop.py
@@ -0,0 +1,16 @@
+# For Loop ต้องระบุจำนวนรอบ
+
+# for counter in range(5):
+#     print("Hello python")
+
+# for counter in range(5): #0-4
+#     print(counter)
+
+# for counter in range(1,5): #1-4
+#     print(counter)
+
+# for counter in range(1,6,2): #1-5 โดยเพิ่มขึ้นทีละ 2
+#     print(counter)
+
+for counter in range(10,0,-1):
+    print(counter)
